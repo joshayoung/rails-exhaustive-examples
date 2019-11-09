a6ddede13aa4eae06f02c03af9461ab7b9fb8f33
Josh Young <j22cal@gmail.com>
2019-11-08 20:28:29 -0600

    Add pending text for degree feature spec

--- spec/features/degree_spec.rb
+++ spec/features/degree_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature "Degrees", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "teachers/:teacher_id/degrees" do
+    it "can see a list of degrees"
+    it "can delete a degree"
+  end
+
+  describe "teachers/:teacher_id/degrees/new" do
+    context "when I visit the degree's new page" do
+      it "can add a new degree"
+    end
+  end
+
+  describe "teachers/:teacher_id/degrees/:id" do
+    context "when I visit the degree's show page" do
+      it "see the degree's details"
+    end
+  end
+
+  describe "teachers/:teacher_id/degrees/:id/edit" do
+    context "when I visit the degree's edit page" do
+      it "can edit the degree"
+    end
+  end
 end
