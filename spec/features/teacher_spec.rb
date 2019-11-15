5235f2f7fee4fc09d18c214c4ce43aa28d3b9aa6
Josh Young <j22cal@gmail.com>
2019-11-14 20:06:25 -0600

    Add more teacher specs

--- spec/features/teacher_spec.rb
+++ spec/features/teacher_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.feature "Teachers", type: :feature do
   describe "teachers/new" do
@@ -74,10 +74,75 @@ RSpec.feature "Teachers", type: :feature do
         expect(page).to have_content("Sue")
       end
 
-      it "can add a degree"
-      it "can add a class"
-      it "show the clases being taught"
-      it "list the degrees attained"
+      it "can add a degree" do
+        visit "teachers"
+
+        within("//table/tbody/tr[1]/td[6]") do
+          click_link "Add Degree"
+        end
+
+        expect(page).to have_content("New Degree")
+      end
+
+      it "can add a class" do
+        visit "teachers"
+
+        within("//table/tbody/tr[1]/td[7]") do
+          click_link "Add Class"
+        end
+
+        expect(page).to have_content("New Class")
+      end
+
+    end
+
+    context "when I visit the teacher show page" do
+      before(:each) do
+        Teacher.create!(name: "Sam", age: 41)
+        Klass.create!(
+          name: "first class",
+          time: Date.today,
+          level: 1,
+          teacher: Teacher.first
+        )
+        Degree.create!(
+          title: "first degree", 
+          level: 2, 
+          completed: Date.yesterday - 1.year,
+          from: "college",
+          teacher: Teacher.first
+        )
+        Student.create!(
+          name: "James Jones",
+          average_grade: 93,
+          class_of: Date.today + 10.years
+        )
+        Registrar.create!(
+          klass: Klass.first, 
+          student: Student.first
+        )
+      end
+
+      it "show the clases being taught" do
+        visit "teachers/#{Teacher.first.id}"
+
+        expect(page).to have_content("Classes:")
+        expect(page).to have_content("first class")
+      end
+
+      it "list the degrees attained" do
+        visit "teachers/#{Teacher.first.id}"
+
+        expect(page).to have_content("Degrees:")
+        expect(page).to have_content("First Degree")
+      end
+
+      it "lists each teacher's students" do
+        visit "teachers/#{Teacher.first.id}"
+
+        expect(page).to have_content("Students:")
+        expect(page).to have_content("James Jones")
+      end
     end
   end
 end
