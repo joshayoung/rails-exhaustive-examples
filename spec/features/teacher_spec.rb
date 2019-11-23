390ddecdbbd443015c0dc55080cd6d73485c36e6
Josh Young <j22cal@gmail.com>
2019-11-23 14:08:17 -0600

    Cleanup teacher spec

--- spec/features/teacher_spec.rb
+++ spec/features/teacher_spec.rb
@@ -1,12 +1,11 @@
 require "rails_helper"
 
+# CRUD
 RSpec.feature "Teachers", type: :feature do
-  describe "teachers/new" do
+  describe "Create" do
     context "when I visit the new teacher page" do
       it "can add a new teacher" do
-        visit "teachers/new"
-
-        expect(page).to have_content("New Teacher")
+        visit new_teacher_path
 
         within("form") do
           fill_in "teacher_name", with: "James"
@@ -18,67 +17,82 @@ RSpec.feature "Teachers", type: :feature do
       end
     end
 
-    context "when I visit the show page" do
-      before(:each) do
-        Teacher.create(name: "Sam", age: 41)
+    describe "Read" do
+      context "when I visit the show page" do
+        before do
+          Teacher.create(name: "Sam", age: 41)
+        end
+        it "can see teacher details" do
+          visit teacher_path(Teacher.first)
+
+          expect(page).to have_content("Name: Sam")
+          expect(page).to have_content("Age: 41")
+        end
       end
-      it "can see teacher details" do
-        visit "teachers/#{Teacher.first.id}"
-
-        expect(page).to have_content("Name: Sam")
-        expect(page).to have_content("Age: 41")
-      end
-    end
-
-    context "when I visit the edit page" do
-      before(:each) do
-        Teacher.create(name: "Sam", age: 41)
-      end
-
-      it "can update the teacher" do
-        teacher = Teacher.first
-
-        visit "teachers/#{teacher.id}/edit"
-
-        within("form") do
-          fill_in "teacher_name", with: "Jack"
-          fill_in "teacher_age", with: "45"
+      context "when I visite the index page" do
+        before(:each) do
+          Teacher.create(name: "Sam", age: 41)
+          Teacher.create(name: "Sue", age: 31)
         end
 
-        click_button "Update Teacher"
+        it "can see a list of teachers" do
+          visit teachers_path
 
-        expect(page).to have_content("Teacher was successfully updated.")
+          expect(page).to have_content("Sam")
+          expect(page).to have_content("Sue")
+        end
       end
     end
 
-    context "when I visit the teachers page" do
+    describe "Update" do
+      context "when I visit the edit page" do
+        it "can fill out form, get success message, and update teacher" do
+          teacher = Teacher.create(name: "Sam", age: 41)
+          visit edit_teacher_path(teacher)
+
+          within("form") do
+            fill_in "teacher_name", with: "Jack"
+            fill_in "teacher_age", with: "45"
+          end
+
+          click_button "Update Teacher"
+
+          expect(page).to have_content("Teacher was successfully updated.")
+
+          teacher.reload
+          expect(teacher.name).to eq("Jack")
+          expect(teacher.age).to eq("45")
+        end
+      end
+    end
+
+    describe "Delete" do
+      context "when I visit the index page" do
+        before(:each) do
+          Teacher.create(name: "Sam", age: 41)
+          Teacher.create(name: "Sue", age: 31)
+        end
+        it "can delete a teacher" do
+          visit teachers_path
+          teacher = Teacher.first
+
+          within("//table/tbody/tr[1]/td[5]") do
+            click_link "Destroy"
+          end
+
+          expect(page).to have_content("Teacher was successfully destroyed.")
+          expect(page).to_not have_content("Sam")
+          expect(page).to have_content("Sue")
+          expect(Teacher.all).not_to include(teacher)
+        end
+      end
+    end
+
+    context "Miscellaneous" do
       before(:each) do
         Teacher.create(name: "Sam", age: 41)
         Teacher.create(name: "Sue", age: 31)
       end
-
-      it "can see a list of teachers" do
-        visit "teachers"
-
-        expect(page).to have_content("Sam")
-        expect(page).to have_content("Sue")
-      end
-
-      it "can delete a teacher" do
-        visit "teachers"
-
-        expect(page).to have_content("Sam")
-        expect(page).to have_content("Sue")
-
-        within("//table/tbody/tr[1]/td[5]") do
-          click_link "Destroy"
-        end
-
-        expect(page).to have_content("Teacher was successfully destroyed.")
-        expect(page).to_not have_content("Sam")
-        expect(page).to have_content("Sue")
-      end
-
       it "can add a degree" do
         visit "teachers"
 
@@ -98,7 +112,6 @@ RSpec.feature "Teachers", type: :feature do
 
         expect(page).to have_content("New Class")
       end
-
     end
 
     context "when I visit the teacher show page" do
@@ -111,8 +124,8 @@ RSpec.feature "Teachers", type: :feature do
           teacher: Teacher.first
         )
         Degree.create!(
-          title: "first degree", 
-          level: 2, 
+          title: "first degree",
+          level: 2,
           completed: Date.yesterday - 1.year,
           from: "college",
           teacher: Teacher.first
@@ -123,7 +136,7 @@ RSpec.feature "Teachers", type: :feature do
           class_of: Date.today + 10.years
         )
         Registrar.create!(
-          klass: Klass.first, 
+          klass: Klass.first,
           student: Student.first
         )
       end
